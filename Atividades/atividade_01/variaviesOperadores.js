53fa53bca68d8a347bb3ddba8a1f13421356a8ce
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-03-18 09:21:39 -0300

    update

--- Atividades/atividade_01/variaviesOperadores.js
+++ Atividades/atividade_01/variaviesOperadores.js
@@ -1,6 +1,20 @@
+/*
+    Instruções
+1 – No repositório de atividades, crie uma pasta atividade1 e crie o arquivo variaveisOperadores.js;
+
+2 – Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável;
+
+3 – Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B}”
+
+Como solução, poste o link da pasta atividade1 no seu repositório de atividades.
+*/
+
 let numero1 = 10
 let numero2 = 20
 let resultado
 
 resultado = numero1 + numero2
-console.log("O resultado da soma entre os numeros 10 e 20 é: ",resultado)
+console.log(`O resultado da soma do ${numero1} e ${numero2} e ${resultado}`)
+console.log(`O resultado da soma do ${numero1} e ${numero2} e ${numero1-numero2}`)
+console.log(`O resultado da soma do ${numero1} e ${numero2} e ${numero1*numero2}`)
+console.log(`O resultado da soma do ${numero1} e ${numero2} e ${numero1/numero2}`)
