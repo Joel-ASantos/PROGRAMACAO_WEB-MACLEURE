d6f7845fedb500221ed1ae9911390e08982e50aa
Joel-ASantos <joel.alves.santos77@gmail.com>
2024-04-05 10:58:36 -0300

    add

---
+++ modulo/util/matriz.js
@@ -0,0 +1,26 @@
+function printMatriz(B) {
+    for(let i = 0; i < B.length; i++){
+        console.log(B[i].join("\t"))
+    }   
+}
+function transporMatriz(A) {
+    console.log("Antes:\n")
+    printMatriz(A)
+    
+    const matrizTransposta = []
+    const linha = A.length
+    const colunas = A[0].length
+
+    for (let j = 0; j < colunas; j++) {
+        matrizTransposta[j] = []    
+    }
+
+    for (let i = 0; i < linha; i++) {
+        for (let j = 0; j < colunas; j++) {
+            matrizTransposta[j][i] = A[i][j]    
+        }   
+    }
+    console.log("\nDepois:\n")
+    printMatriz(matrizTransposta)
+}
+module.exports = {transporMatriz}
